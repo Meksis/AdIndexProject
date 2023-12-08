ad95fcbe1beb9217d7ca45865c34cc3817b00d88
Meksis <orlov.work.gen@gmail.com>
2023-12-08 22:46:50 +0300

    Еще доработал парсер

--- Utils/parser.py
+++ Utils/parser.py
@@ -51,6 +51,7 @@ def parse_some_news(driver : webdriver.Firefox = None, days4parse : int = 1) -> 
             tag = footer_object.find_element(By.TAG_NAME, 'a').text
             date = footer_object.find_element(By.TAG_NAME, 'span').text.split(' | ')
             date = date[0] if len(date[0]) > len(date[1]) else date[1]
+            date = '.'.join(date.split('.')[ : -1]) + '.20' + date.split('.')[-1]
 
 
             id = link[link.index('.phtml') - 6 : link.index('.phtml')]
@@ -68,8 +69,6 @@ def parse_some_news(driver : webdriver.Firefox = None, days4parse : int = 1) -> 
             
             posts_datas[-1].update({'views' : random.randint(round(posts_datas[-1]['visitors']), round(posts_datas[-1]['visitors'] + posts_datas[-1]['visitors'] * .5))})
 
-            # print(posts_datas[-1], date, id)
-
             if not date_prev:
                 date_prev = str(date)
             
@@ -79,19 +78,7 @@ def parse_some_news(driver : webdriver.Firefox = None, days4parse : int = 1) -> 
                     days_iterated += 1
 
 
-        posts_datas = pd.DataFrame(posts_datas)
-        posts_datas['date'] = pd.to_datetime(posts_datas['date'], format='mixed')
-
-        # try:
-        #     posts_datas['date'] = pd.to_datetime(posts_datas['date'], format='mixed')
-        
-        # except Exception as e:
-        #     print(posts_datas)
-        #     print(date, id)
-        #     raise e
-
-
-        # posts_datas['date'] = pd.to_datetime(posts_datas['date'], format='%d.%m.%y')
+        posts_datas = pd.DataFrame.from_dict(posts_datas)
 
         if isinstance(posts_total_info, list):
             posts_total_info = posts_datas.copy()
@@ -122,12 +109,13 @@ def parse_some_news(driver : webdriver.Firefox = None, days4parse : int = 1) -> 
             tag = footer_object.find_element(By.TAG_NAME, 'a').text
             date = footer_object.find_element(By.TAG_NAME, 'span').text.split(' | ')
             date = date[0] if len(date[0]) > len(date[1]) else date[1]
+            date = '.'.join(date.split('.')[ : -1]) + '.20' + date.split('.')[-1]
 
             
 
             id = link[link.index('.phtml') - 6 : link.index('.phtml')]
 
-            print(date, id)
+            # print(date, id)
             
 
             posts_datas.append({
@@ -146,11 +134,10 @@ def parse_some_news(driver : webdriver.Firefox = None, days4parse : int = 1) -> 
                 date_changed = True
 
 
-        posts_datas = pd.DataFrame(posts_datas)
-        posts_datas['date'] = pd.to_datetime(posts_datas['date'], format='mixed')
-        # posts_datas['date'] = pd.to_datetime(posts_datas['date'], format='%d.%m.%y')
+        posts_datas = pd.DataFrame.from_dict(posts_datas)
+        # posts_datas.date = pd.to_datetime(posts_datas.date, format='%d.%m.%y')
 
-        posts_datas = posts_datas[posts_datas['date'] != date]
+        posts_datas = posts_datas[posts_datas.date != date]
 
         if isinstance(posts_total_info, list):
             posts_total_info = posts_datas.copy()
@@ -163,10 +150,13 @@ def parse_some_news(driver : webdriver.Firefox = None, days4parse : int = 1) -> 
         show_more_button.click()
         button_clicked += 1
 
-        # time.sleep(1)
+        time.sleep(1)
         
     
     driver.close()
+    
+    posts_total_info.date = pd.to_datetime(posts_total_info.date, dayfirst=True, format='%d.%m.%Y')
+
 
     posts_total_info.to_csv(f'AdIndex main news {datetime.datetime.today().strftime("%Y-%m-%d")} - {days4parse} days.csv')
     return(posts_total_info)
---
+++ Utils/parser_testing.py
@@ -0,0 +1,3 @@
+import parser
+result = parser.parse_some_news(days4parse=2)
+print(result.date)

a06659b7bf4d7d2b5ce22dcd6fb2c802cb05ec30
YourPersonalClown <daniel13hj@gmail.com>
2023-12-13 23:18:03 +0300

    d

--- 1_Главная.py
+++ 1_Главная.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import datetime
 
 from Utils.parser import *
+
 st.set_page_config(layout="wide", initial_sidebar_state='collapsed') # установка широкого расположения объектов
 
 # Загрузка данных (замените 'your_data.csv' на путь к вашему файлу данных)
@@ -29,6 +30,8 @@ else:
 # Создание списка уникальных дат для выбора периода
 date_list = sorted(data['date'].unique())
 
+today = datetime.datetime.now()
+
 # Заголовок
 st.title('Графики и диаграммы с выбором периода')
 
@@ -38,22 +41,13 @@ if st.button('Обновить данные'):
         st.session_state['datas'] = parse_some_news()
         data = st.session_state['datas']
 
-
 # Разделение экрана на три столбца
-col1, _, col3 = st.columns(3)
 
+val1, _, val2 = st.columns([0.3,0.4, 0.3])
 
+with val1:
 
-
-# График во втором столбце
-with col1:
-    # Выпадающий список для выбора даты в первом столбце
-    # selected_date_scatter = col1.selectbox('Выберите дату', [date.date() for date in date_list], key='scatter_selector')
-
-    today = datetime.datetime.now()
-
-    try:
-        selected_date_scatter = st.date_input(      # Создаем поле для ввода даты, задавая размеры от минимальной даты в датафрейме до текущей и выставляя значение от текущей недели до 7 дней назад
+    selected_date_scatter = st.date_input(      # Создаем поле для ввода даты, задавая размеры от минимальной даты в датафрейме до текущей и выставляя значение от текущей недели до 7 дней назад
             "Выберете промежуток выборки",
             (
                 datetime.date(today.year, today.month, today.day - 7 if today.day - 7 > 0 else 1),
@@ -63,70 +57,101 @@ with col1:
             # data.date.iloc[-1],
             today,
             format="YYYY.MM.DD",
-            key='scatter_datepick'
-        )
+            key='scatter_datepick')
 
 
-        filtered_data_scatter = data[data['date'] <= pd.Timestamp(selected_date_scatter[1].strftime("%Y.%d.%m"))][data['date'] >= pd.Timestamp(selected_date_scatter[0].strftime("%Y.%d.%m"))]   
+with val2:
+        
+        selected_metric_diagram = val2.selectbox('Выберите метрику для сравнения', ['date', 'author', 'visitors', 'views'] )
 
 
-        # st.write(pd.DataFrame({'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() }, index=['Средняя глубина просмотра'], columns = ['Средняя глубина просмотра'] ))
-        # st.write(pd.DataFrame.from_records([{'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() }]))
-
-        # print(pd.DataFrame.from_records([{'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() }]))
-
-        st.table({'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() })
+st.table(data)
 
 
-        # Фильтрация данных по выбранной дате
-        fig_line = px.scatter(filtered_data_scatter, x='date', y='visitors', title='График')
-        st.plotly_chart(fig_line)
 
 
-    except Exception as e:
-        pass
+
+
+# col1, _, col3 = st.columns(3)
+
+# # График во втором столбце
+# with col1:
+#     # Выпадающий список для выбора даты в первом столбце
+#     # selected_date_scatter = col1.selectbox('Выберите дату', [date.date() for date in date_list], key='scatter_selector')
+
+
+# # try:
+        
+
+
+#         filtered_data_scatter = data[data['date'] <= pd.Timestamp(selected_date_scatter[1].strftime("%Y.%m.%d"))][data['date'] >= pd.Timestamp(selected_date_scatter[0].strftime("%Y.%m.%d"))]   
+
+
+#         # st.write(pd.DataFrame({'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() }, index=['Средняя глубина просмотра'], columns = ['Средняя глубина просмотра'] ))
+#         # st.write(pd.DataFrame.from_records([{'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() }]))
+
+#         # print(pd.DataFrame.from_records([{'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() }]))
+
+#         st.table({'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() })
+
+
+#         # Фильтрация данных по выбранной дате
+#         fig_line = px.scatter(filtered_data_scatter, x='date', y='visitors', title='График')
+#         st.plotly_chart(fig_line)
+
+
+# # except Exception as e:
+# #     print('ошибка Вадимки try except фембойчика')
+# #     print(e)
 
 
     
 
-# Круговая диаграмма в третьем столбце
-with col3:
-    # Сделать выбор промежутка по времени
-    # selected_date_diagram = col3.selectbox('Выберите дату', date_list, key='diagram_selector')
+# # Круговая диаграмма в третьем столбце
+# with col3:
+#     # Сделать выбор промежутка по времени
+#     # selected_date_diagram = col3.selectbox('Выберите дату', date_list, key='diagram_selector')
     
 
-    today = datetime.datetime.now()
 
-    try:
-        selected_date_diagram = st.date_input(      # Создаем поле для ввода даты, задавая размеры от минимальной даты в датафрейме до текущей и выставляя значение от текущей недели до 7 дней назад
-            "Выберете промежуток выборки",
-            (
-                datetime.date(today.year, today.month, today.day - 7 if today.day - 7 > 0 else 1),
-                today
-            ),
-            datetime.date(data.date.iloc[0].year, data.date.iloc[0].month, data.date.iloc[0].day - 7),
-            # data.date.iloc[-1],
-            today,
-            format="YYYY.MM.DD",
-            key='diagram_datepick'
-        )
-
-
-        # new_datas = data.query(f'date <= {pd.to_datetime(selected_date_diagram[1], format="mixed")} & date >= {pd.to_datetime(selected_date_diagram[0], format="mixed")}')
-
-        selected_metric_diagram = col3.selectbox('Выберите метрику для сравнения', ['date', 'author', 'visitors', 'views'], key='metric_selector' )
-
-        # Фильтрация данных по выбранной дате
-        filtered_data_diagram = data[data['date'] <= pd.Timestamp(selected_date_diagram[1].strftime("%Y.%d.%m"))][data['date'] >= pd.Timestamp(selected_date_diagram[0].strftime("%Y.%d.%m"))]        # Строки с датами между двух выбранных
+#     try:
+#         selected_date_diagram = selected_date_scatter
+#         # st.date_input(      # Создаем поле для ввода даты, задавая размеры от минимальной даты в датафрейме до текущей и выставляя значение от текущей недели до 7 дней назад
+#         #     "Выберете промежуток выборки",
+#         #     (
+#         #         datetime.date(today.year, today.month, today.day - 7 if today.day - 7 > 0 else 1),
+#         #         today
+#         #     ),
+#         #     datetime.date(data.date.iloc[0].year, data.date.iloc[0].month, data.date.iloc[0].day - 7),
+#         #     # data.date.iloc[-1],
+#         #     today,
+#         #     format="YYYY.MM.DD",
+#         #     key='diagram_datepick'
+#         # )
 
 
+#         # new_datas = data.query(f'date <= {pd.to_datetime(selected_date_diagram[1], format="mixed")} & date >= {pd.to_datetime(selected_date_diagram[0], format="mixed")}')
 
-        # fig_pie = px.pie(filtered_data_diagram, names='visitors', title='Круговая диаграмма')
-        fig_pie = px.pie(filtered_data_diagram, names=selected_metric_diagram, title='Круговая диаграмма')
-        st.plotly_chart(fig_pie)
+
+#         # Фильтрация данных по выбранной дате
+#         filtered_data_diagram = data[
+#             data['date'] <= pd.Timestamp(
+#                 selected_date_diagram[1].strftime("%Y.%m.%d")
+#                 )][
+#                     data['date'] >= pd.Timestamp(
+#                         selected_date_diagram[0].strftime("%Y.%m.%d")
+#                         )]        # Строки с датами между двух выбранных
+
+
+
+#         # fig_pie = px.pie(filtered_data_diagram, names='visitors', title='Круговая диаграмма')
+#         fig_pie = px.pie(filtered_data_diagram, names=selected_metric_diagram, title='Круговая диаграмма')
+#         st.plotly_chart(fig_pie)
     
-    except Exception as e:
-        raise e
-        # print(str(e))
+#     except Exception as e:
+#         print('ошибка Вадимки try except фембойчика часть 2')
+#         print(e)
+
+
 
 

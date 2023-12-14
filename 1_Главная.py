f43664453565902de7fd7d041bead9423c12429a
Meksis <orlov.work.gen@gmail.com>
2023-12-14 04:22:51 +0300

    Масса изменений
    - Доработан парсер ( ToDo: изменить способ счета дней, хотя можно и без этого, поскольку из датафрейма берутся данные по конкретным датам)
    - Добавить графиков

--- 1_Главная.py
+++ 1_Главная.py
@@ -5,22 +5,18 @@ import plotly.express as px
 import datetime
 
 from Utils.parser import *
-
 st.set_page_config(layout="wide", initial_sidebar_state='collapsed') # установка широкого расположения объектов
+
+
+selected_date = None
+
 
 # Загрузка данных (замените 'your_data.csv' на путь к вашему файлу данных)
 # data = pd.read_csv('your_data.csv')
 
-#CREATE TABLE DATA 
-    # Date date,
-    # author TEXT,
-    # visitors INTEGER,
-    # post_tag TEXT,
-    # link TEXT,
-    # post_id TEXT
 
 if 'datas' not in st.session_state:
-    st.session_state['datas'] = parse_some_news()
+    st.session_state['datas'] = parse_some_news(days4parse=7)
     data = st.session_state['datas']
 
 else:
@@ -30,128 +26,99 @@ else:
 # Создание списка уникальных дат для выбора периода
 date_list = sorted(data['date'].unique())
 
-today = datetime.datetime.now()
-
 # Заголовок
 st.title('Графики и диаграммы с выбором периода')
 
 
 if st.button('Обновить данные'):
     with st.spinner('Обновляемся'):
-        st.session_state['datas'] = parse_some_news()
+        st.session_state['datas'] = parse_some_news(days4parse=(st.session_state['picked_dates'][1] - st.session_state['picked_dates'][0]).days)
         data = st.session_state['datas']
 
+
+
+today = datetime.datetime.now()
+
+try:
+    selected_date = st.date_input(      # Создаем поле для ввода даты, задавая размеры от минимальной даты в датафрейме до текущей и выставляя значение от текущей недели до 7 дней назад
+        "Выберете промежуток выборки",
+        (
+            datetime.date(today.year, today.month, today.day - 7 if today.day - 7 > 0 else 1),
+            today
+        ),
+        datetime.date(data.date.iloc[0].year, data.date.iloc[0].month, data.date.iloc[0].day - 7),
+        # data.date.iloc[-1],
+        today,
+        format="YYYY.MM.DD",
+        key='scatter_datepick'
+    )
+
+    st.session_state['picked_dates'] = [selected_date[0], selected_date[1]]
+
+
+    filtered_data = data[data['date'] <= pd.Timestamp(selected_date[1].strftime("%Y.%m.%d"))][data['date'] >= pd.Timestamp(selected_date[0].strftime("%Y.%m.%d"))]   
+
+
+    # st.write(pd.DataFrame({'Средняя глубина просмотра' : filtered_data['visitors'].sum() / filtered_data['views'].sum() }, index=['Средняя глубина просмотра'], columns = ['Средняя глубина просмотра'] ))
+    # st.write(pd.DataFrame.from_records([{'Средняя глубина просмотра' : filtered_data['visitors'].sum() / filtered_data['views'].sum() }]))
+
+    # print(pd.DataFrame.from_records([{'Средняя глубина просмотра' : filtered_data['visitors'].sum() / filtered_data['views'].sum() }]))
+
+    st.table({'Средняя глубина просмотра' : filtered_data['visitors'].sum() / filtered_data['views'].sum() })
+
+
+except Exception as e:
+    pass
+
+
 # Разделение экрана на три столбца
-
-val1, _, val2 = st.columns([0.3,0.4, 0.3])
-
-with val1:
-
-    selected_date_scatter = st.date_input(      # Создаем поле для ввода даты, задавая размеры от минимальной даты в датафрейме до текущей и выставляя значение от текущей недели до 7 дней назад
-            "Выберете промежуток выборки",
-            (
-                datetime.date(today.year, today.month, today.day - 7 if today.day - 7 > 0 else 1),
-                today
-            ),
-            datetime.date(data.date.iloc[0].year, data.date.iloc[0].month, data.date.iloc[0].day - 7),
-            # data.date.iloc[-1],
-            today,
-            format="YYYY.MM.DD",
-            key='scatter_datepick')
-
-
-with val2:
-        
-        selected_metric_diagram = val2.selectbox('Выберите метрику для сравнения', ['date', 'author', 'visitors', 'views'] )
-
-
-st.table(data)
+col1, _, col3 = st.columns(3)
 
 
 
+container = st.container(border=True)
+container.write("This is inside the container")
+st.write("This is outside the container")
+
+# Now insert some more in the container
+container.write("This is inside too")
 
 
+# График во втором столбце
 
-# col1, _, col3 = st.columns(3)
-
-# # График во втором столбце
 # with col1:
-#     # Выпадающий список для выбора даты в первом столбце
-#     # selected_date_scatter = col1.selectbox('Выберите дату', [date.date() for date in date_list], key='scatter_selector')
-
-
-# # try:
+#     container = st.container(border=True)
+#     with container:
+#         # Выпадающий список для выбора даты в первом столбце
+#         # selected_date_scatter = col1.selectbox('Выберите дату', [date.date() for date in date_list], key='scatter_selector')
+#             # Фильтрация данных по выбранной дате
+            
+#         try:
+#             fig_line = px.bar(filtered_data, x='date', y='visitors', title='График')
+#             st.plotly_chart(fig_line)
         
+#         except Exception as e:
+#             print(e)
 
 
-#         filtered_data_scatter = data[data['date'] <= pd.Timestamp(selected_date_scatter[1].strftime("%Y.%m.%d"))][data['date'] >= pd.Timestamp(selected_date_scatter[0].strftime("%Y.%m.%d"))]   
-
-
-#         # st.write(pd.DataFrame({'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() }, index=['Средняя глубина просмотра'], columns = ['Средняя глубина просмотра'] ))
-#         # st.write(pd.DataFrame.from_records([{'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() }]))
-
-#         # print(pd.DataFrame.from_records([{'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() }]))
-
-#         st.table({'Средняя глубина просмотра' : filtered_data_scatter['visitors'].sum() / filtered_data_scatter['views'].sum() })
-
-
-#         # Фильтрация данных по выбранной дате
-#         fig_line = px.scatter(filtered_data_scatter, x='date', y='visitors', title='График')
-#         st.plotly_chart(fig_line)
-
-
-# # except Exception as e:
-# #     print('ошибка Вадимки try except фембойчика')
-# #     print(e)
+    
 
 
     
 
 # # Круговая диаграмма в третьем столбце
 # with col3:
-#     # Сделать выбор промежутка по времени
-#     # selected_date_diagram = col3.selectbox('Выберите дату', date_list, key='diagram_selector')
-    
+#     with st.container(border=True):
+#         # Сделать выбор промежутка по времени
+#         # selected_date_diagram = col3.selectbox('Выберите дату', date_list, key='diagram_selector')
+        
 
-
-#     try:
-#         selected_date_diagram = selected_date_scatter
-#         # st.date_input(      # Создаем поле для ввода даты, задавая размеры от минимальной даты в датафрейме до текущей и выставляя значение от текущей недели до 7 дней назад
-#         #     "Выберете промежуток выборки",
-#         #     (
-#         #         datetime.date(today.year, today.month, today.day - 7 if today.day - 7 > 0 else 1),
-#         #         today
-#         #     ),
-#         #     datetime.date(data.date.iloc[0].year, data.date.iloc[0].month, data.date.iloc[0].day - 7),
-#         #     # data.date.iloc[-1],
-#         #     today,
-#         #     format="YYYY.MM.DD",
-#         #     key='diagram_datepick'
-#         # )
-
-
-#         # new_datas = data.query(f'date <= {pd.to_datetime(selected_date_diagram[1], format="mixed")} & date >= {pd.to_datetime(selected_date_diagram[0], format="mixed")}')
-
-
-#         # Фильтрация данных по выбранной дате
-#         filtered_data_diagram = data[
-#             data['date'] <= pd.Timestamp(
-#                 selected_date_diagram[1].strftime("%Y.%m.%d")
-#                 )][
-#                     data['date'] >= pd.Timestamp(
-#                         selected_date_diagram[0].strftime("%Y.%m.%d")
-#                         )]        # Строки с датами между двух выбранных
-
-
+#         selected_metric_diagram = col3.selectbox('Выберите метрику для сравнения', ['date', 'author', 'visitors', 'views'], key='metric_selector' )
 
 #         # fig_pie = px.pie(filtered_data_diagram, names='visitors', title='Круговая диаграмма')
-#         fig_pie = px.pie(filtered_data_diagram, names=selected_metric_diagram, title='Круговая диаграмма')
-#         st.plotly_chart(fig_pie)
-    
-#     except Exception as e:
-#         print('ошибка Вадимки try except фембойчика часть 2')
-#         print(e)
-
-
-
-
+#         try:
+#             fig_pie = px.pie(filtered_data, names=selected_metric_diagram, title='Круговая диаграмма')
+#             st.plotly_chart(fig_pie)
+        
+#         except Exception as e:
+#             print(e)
--- Utils/parser.py
+++ Utils/parser.py
@@ -3,7 +3,10 @@ from selenium.webdriver.common.by import By
 
 import pandas as pd
 import random
-
+import datetime
+import time
+import requests
+from statistics import median
 
 main_url = 'https://adindex.ru/news/all.phtml'
 authors_names = [
@@ -20,68 +23,236 @@ authors_names = [
 ]
 
 
-def parse_some_news(driver : webdriver.Firefox = None) -> pd.DataFrame: # type: ignore
+
+
+
+def parse_some_news(driver : webdriver.Firefox = None, days4parse : int = 1) -> pd.DataFrame: # type: ignore
     if not driver:
         driver = webdriver.Firefox()
 
-
-    driver.get(main_url)
-    posts_cards = driver.find_elements(By.CLASS_NAME, 'newsfeed__list-item ')
-
+    days_iterated = 0
+    button_clicked = 0
+    posts_total_info = []
+    date_prev = ''
     posts_datas = []
 
-    for element in posts_cards:
-        link = element.find_element(By.CLASS_NAME, 'newsfeed__list-item-title').find_element(By.TAG_NAME, 'a').get_attribute('href')
+    driver.get(main_url)
+
+    while days_iterated != days4parse:
+        if isinstance(posts_datas, list):
+            posts_cards = driver.find_elements(By.CLASS_NAME, 'newsfeed__list-item ')
         
-        footer_object = element.find_element(By.CLASS_NAME, 'newsfeed__list-footer')
-        tag = footer_object.find_element(By.TAG_NAME, 'a').text
-        date = footer_object.find_element(By.TAG_NAME, 'span').text.split(' | ')[0]
+        else:
+            posts_cards = driver.find_elements(By.CLASS_NAME, 'newsfeed__list-item ')[posts_datas.post_id.size * button_clicked : ]
 
+
+        posts_datas = []
+
+        for element in posts_cards:
+            link = element.find_element(By.CLASS_NAME, 'newsfeed__list-item-title').find_element(By.TAG_NAME, 'a').get_attribute('href')
+            
+            footer_object = element.find_element(By.CLASS_NAME, 'newsfeed__list-footer')
+            tag = footer_object.find_element(By.TAG_NAME, 'a').text
+            date = footer_object.find_element(By.TAG_NAME, 'span').text.split(' | ')
+            date = date[0] if len(date[0]) > len(date[1]) else date[1]
+            date = '.'.join(date.split('.')[ : -1]) + '.20' + date.split('.')[-1]
+
+
+            id = link[link.index('.phtml') - 6 : link.index('.phtml')]
+
+
+
+            posts_datas.append({
+                'date' : date,
+                'author' : random.choice(authors_names),
+                'visitors' : random.randint(100, 20000),
+                'post_tag' : tag,
+                'post_id' : id,
+                'link' : link
+            })
+            
+            posts_datas[-1].update({'views' : random.randint(round(posts_datas[-1]['visitors']), round(posts_datas[-1]['visitors'] + posts_datas[-1]['visitors'] * .5))})
+
+            if not date_prev:
+                date_prev = str(date)
+            
+            else:
+                if date != date_prev:
+                    date_prev = str(date)
+                    days_iterated += 1
+
+
+        posts_datas = pd.DataFrame.from_dict(posts_datas)
+
+        if isinstance(posts_total_info, list):
+            posts_total_info = posts_datas.copy()
+
+        else:
+            posts_total_info = pd.concat([posts_total_info, posts_datas], ignore_index = True)
+
+
+        show_more_button = driver.find_element(By.CLASS_NAME, 'js-load-news')
+        show_more_button.click()
+        button_clicked += 1
+
+        time.sleep(1)
+
+    
+    date_changed = False
+
+    while not date_changed:
+        posts_cards = driver.find_elements(By.CLASS_NAME, 'newsfeed__list-item ')[posts_datas.post_id.size * button_clicked : ]
+
+
+        posts_datas = []
+
+        for element in posts_cards:
+            link = element.find_element(By.CLASS_NAME, 'newsfeed__list-item-title').find_element(By.TAG_NAME, 'a').get_attribute('href')
+            
+            footer_object = element.find_element(By.CLASS_NAME, 'newsfeed__list-footer')
+            tag = footer_object.find_element(By.TAG_NAME, 'a').text
+            date = footer_object.find_element(By.TAG_NAME, 'span').text.split(' | ')
+            date = date[0] if len(date[0]) > len(date[1]) else date[1]
+            date = '.'.join(date.split('.')[ : -1]) + '.20' + date.split('.')[-1]
+
+            
+
+            id = link[link.index('.phtml') - 6 : link.index('.phtml')]
+
+            # print(date, id)
+            
+
+            posts_datas.append({
+                'date' : date,
+                'author' : random.choice(authors_names),
+                'visitors' : random.randint(100, 20000),
+                'post_tag' : tag,
+                'post_id' : id,
+                'link' : link
+            })
+            
+            posts_datas[-1].update({'views' : random.randint(round(posts_datas[-1]['visitors']), round(posts_datas[-1]['visitors'] + posts_datas[-1]['visitors'] * .5))})
+            
+            if date != date_prev:
+                date_prev = str(date)
+                date_changed = True
+
+
+        posts_datas = pd.DataFrame.from_dict(posts_datas)
+        # posts_datas.date = pd.to_datetime(posts_datas.date, format='%d.%m.%y')
+
+        posts_datas = posts_datas[posts_datas.date != date]
+
+        if isinstance(posts_total_info, list):
+            posts_total_info = posts_datas.copy()
+
+        else:
+            posts_total_info = pd.concat([posts_total_info, posts_datas], ignore_index = True)
+
+
+        show_more_button = driver.find_element(By.CLASS_NAME, 'js-load-news')
+        show_more_button.click()
+        button_clicked += 1
+
+        time.sleep(1)
         
-
-        id = link[link.index('.phtml') - 6 : link.index('.phtml')]
-
-
-        posts_datas.append({
-            'date' : date,
-            'author' : random.choice(authors_names),
-            'visitors' : random.randint(100, 20000),
-            'post_tag' : tag,
-            'post_id' : id,
-            'link' : link
-        })
-        
-        posts_datas[-1].update({'views' : random.randint(round(posts_datas[-1]['visitors']), round(posts_datas[-1]['visitors'] + posts_datas[-1]['visitors'] * .5))})
     
     driver.close()
-
-    posts_datas = pd.DataFrame(posts_datas)
-    posts_datas['date'] = pd.to_datetime(posts_datas['date'], format='mixed')
-
-    posts_datas.to_csv(f'AdIndex main news {date}')
-    return(posts_datas)
-
-import requests
+    
+    posts_total_info.date = pd.to_datetime(posts_total_info.date, dayfirst=True, format='%d.%m.%Y')
 
 
-def get_data_from_url(url: str ):
+    posts_total_info.to_csv(f'AdIndex main news {datetime.datetime.today().strftime("%Y-%m-%d")} - {days4parse} days.csv')
+    return(posts_total_info)
+
+
+
+
+def get_data_from_url(url: str, days_from : str = "yesterday", days_to : str = "today"):
     params = {
         'ids': '22386646',
         # 'metrics': 'ym:s:visits,ym:s:pageviews,ym:s:users,ym:s:bounceRate,ym:s:pageDepth,ym:s:avgVisitDurationSeconds',
-        'metrics': 'ym:s:pageviews',  # Метрика для просмотров страниц
-        'dimensions': 'ym:s:referer',  # Размерность для получения ссылок
-        'date1': 'today',  # 7daysAgo за неделю, 30daysAgo за месяц, 365daysAgo за год
-        'date2': 'today',
+        # 'metrics': 'ym:s:pageviews',  # Метрика для просмотров страниц
+        'metrics': 'ym:s:visits,ym:s:pageviews,ym:s:users,ym:s:bounceRate,ym:s:pageDepth,ym:s:avgVisitDurationSeconds',
+        'dimensions': 'ym:s:refererDomain',  # Размерность для получения ссылок
+        'date1': days_from,  # 7daysAgo за неделю, 30daysAgo за месяц, 365daysAgo за год
+        'date2': days_to,
         'filters': f"ym:pv:URL=='{url}'"
     }
 
-    url = 'https://api-metrika.yandex.net/stat/v1/data'
+    api_url = 'https://api-metrika.yandex.net/stat/v1/data'
 
 
     headers = {
         'Authorization': 'OAuth y0_AgAAAABxT5u9AArjtwAAAADzHTIKv-VGe5uzTHqKqd3WWTTeIiZQ2WU',
     }
 
-    response = requests.get(url, headers=headers, params=params )
-    return response.json()
+    response = requests.get(api_url, headers=headers, params=params ).json()
 
+    if not response['data']:
+        return response, url
+
+
+    page_metrics = []
+    response_data = response['data']
+    names = ['Посетители', "Читатели", "Уникальные пользователи", "% недочитываемости", "Глубина просмотра", "Ср. время на сайте (сек)"]
+
+
+    for data in response_data:
+        page_metrics.append(
+            {
+                'Сайт' : data['dimensions'][0]['name']
+            }
+        )
+
+        for counter, metric_name in enumerate(names):
+            page_metrics[-1].update(
+                {
+                    metric_name : data['metrics'][counter]
+                }
+            )
+    out = [{}, {}, {'% недочитываемости' : [], 'Глубина просмотра' : [], "Ср. время на сайте (сек)"  : []}]
+
+    restrict_keys = ['Сайт', '% недочитываемости', 'Глубина просмотра', "Ср. время на сайте (сек)"]
+
+
+    for data in page_metrics:
+        if 'telegram' not in data['Сайт']:
+            if out[0]:
+                out[0].update(
+                    {
+                        key : out[0][key] + data[key] for key in list(data.keys())[ : -3][1 :]
+                    } 
+                )
+            
+            else:
+                for key in data:
+                    if key != 'Сайт':
+                        out[0][key] = data[key]
+
+
+            for key in restrict_keys[1 :]:
+                out[2][key].append(data[key])
+
+        else:
+            for key in data:
+                if key != 'Сайт':
+                    out[1][key] = round(data[key], 2)
+
+    for key in out[2]:
+        out[0][key] = median(out[2][key])
+
+    del out[2]
+
+
+    out[0] = {
+        'Поисковые системы' : out[0]
+    }
+
+    out[1] = {
+        'Telegram' : out[1]
+    }
+
+
+    # return response.json()
+    return out

55fe2eb52bffe462056b06395ebba5f1c2854fea
Meksis <orlov.work.gen@gmail.com>
2024-04-26 11:10:39 +0300

    Эксрекментирую со страницами

--- dash/app.py
+++ dash/app.py
@@ -1,4 +1,5 @@
-from dash import Dash, dcc, html, Input, Output, callback
+from dash import Dash, dcc, html, Input, Output, callback_context
+import dash
 from dash.dash_table import DataTable
 
 
@@ -7,88 +8,103 @@ import plotly.express as px
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__, external_stylesheets=external_stylesheets, use_pages=True, pages_folder='pages')
 
-reload_btn = html.Button(
-        'reload',
-        '1',
-        0,
-        draggable = 'false'
+# reload_btn = html.Button(
+#         'reload',
+#         '1',
+#         0,
+#         draggable = 'false'
 
-    )
+#     )
+
+
+# plot = dcc.Graph(
+#     'graph'
+# )
+
+# data_table = DataTable(
+#     page_size=10,
+#     style_data_conditional=[{
+#         'if': {'column_editable': False},
+#         'backgroundColor': 'rgb(30, 30, 30)',
+#         'color': 'white'
+#     }],
+#     style_header_conditional=[{
+#         'if': {'column_editable': False},
+#         'backgroundColor': 'rgb(30, 30, 30)',
+#         'color': 'white'
+#     }]
+# )
+
+
+# app.layout = html.Div(
+#     style = {
+#         'bgcolor': 'rgb(0, 0, 0)'
+#     },
+#     id = 'div1',
+#     children = [
+#     reload_btn,
+#     data_table,
+#     plot
+# ]
+    
+# )
+
+
+
+
+
+# @callback(
+#     Output(plot, 'figure'),
+#     Output(data_table, 'data'),
+#     Input(reload_btn, 'n_clicks')
+# )
+# def on_reload_click(n_clicks):
+#     df = pd.read_csv('./csvs/AdIndex main news METRICS 2023-12-14.csv', index_col=0, parse_dates=True)
+#     df['date'] = df['date'].apply(lambda x: pd.to_datetime(x))
+
+#     return (px.line(df['post_id']), df.to_dict('records'))
+
+
+# @callback(
+#     Output(plot, 'figure'),
+#     Input(data_table, 'data'),
+#     Input(reload_btn, 'n_clicks')
+# )
+# def on_reload_click(n_clicks):
+#     df = pd.read_csv('./csvs/AdIndex main news METRICS 2023-12-14.csv', index_col=0, parse_dates=True)
+#     df['date'] = df['date'].apply(lambda x: pd.to_datetime(x))
+
+#     return (px.line(df['post_id']), df.to_dict('records'))
+
+
+
+# @callback(
+#     Output(plot, 'figure'),
+#     Input(data_table, 'data'),
+#     Input(data_table, 'columns')
+# )
+# def callback_table(rows, columns):
+#     df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
 
 
 plot = dcc.Graph(
     'graph'
 )
 
-data_table = DataTable(
-    page_size=10,
-    style_data_conditional=[{
-        'if': {'column_editable': False},
-        'backgroundColor': 'rgb(30, 30, 30)',
-        'color': 'white'
-    }],
-    style_header_conditional=[{
-        'if': {'column_editable': False},
-        'backgroundColor': 'rgb(30, 30, 30)',
-        'color': 'white'
-    }]
-)
-
-
 app.layout = html.Div(
+    
     style = {
         'bgcolor': 'rgb(0, 0, 0)'
     },
     id = 'div1',
     children = [
-    reload_btn,
-    data_table,
+    dash.page_container,
     plot
 ]
     
 )
-
-
-
-
-
-@callback(
-    Output(plot, 'figure'),
-    Output(data_table, 'data'),
-    Input(reload_btn, 'n_clicks')
-)
-def on_reload_click(n_clicks):
-    df = pd.read_csv('./csvs/AdIndex main news METRICS 2023-12-14.csv', index_col=0, parse_dates=True)
-    df['date'] = df['date'].apply(lambda x: pd.to_datetime(x))
-
-    return (px.line(df['post_id']), df.to_dict('records'))
-
-
-@callback(
-    Output(plot, 'figure'),
-    Input(data_table, 'data'),
-    Input(reload_btn, 'n_clicks')
-)
-def on_reload_click(n_clicks):
-    df = pd.read_csv('./csvs/AdIndex main news METRICS 2023-12-14.csv', index_col=0, parse_dates=True)
-    df['date'] = df['date'].apply(lambda x: pd.to_datetime(x))
-
-    return (px.line(df['post_id']), df.to_dict('records'))
-
-
-
-@callback(
-    Output(plot, 'figure'),
-    Input(data_table, 'data'),
-    Input(data_table, 'columns')
-)
-def callback_table(rows, columns):
-    df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
-
-
-
 
 
 
---
+++ dash/pages/main.py
@@ -0,0 +1,50 @@
+from dash import Dash, dcc, html, Input, Output, callback
+import dash
+from dash.dash_table import DataTable
+# import dash_core_components as dcc
+# import dash_html_components as html
+import pandas as pd
+
+dash.register_page(__name__)
+
+# Создайте датафрейм Pandas
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'])
+
+# Создайте приложение Dash
+# app = dash.Dash(__name__)
+
+# Определите layout
+layout = html.Div([
+    html.H1('Демонстрационная страница Dash'),
+    
+    # Таблица
+    DataTable(data=df.to_dict('records')),
+    
+    # График 1 - гистограмма площади лепестков
+    dcc.Graph(
+        id='petal-area-histogram',
+        figure={
+            'data': [
+                {'x': df['petal_length'] * df['petal_width'], 'type': 'histogram'}
+            ],
+            'layout': {
+                'title': 'Гистограмма площади лепестков'
+            }
+        }
+    ),
+    
+    # График 2 - рассеянный диаграмм Sepal Length vs Sepal Width
+    dcc.Graph(
+        id='sepal-scatter',
+        figure={
+            'data': [
+                {'x': df['sepal_length'], 'y': df['sepal_width'], 'mode': 'markers', 'name': 'Sepal Length vs Sepal Width'}
+            ],
+            'layout': {
+                'title': 'Sepal Length vs Sepal Width (рассеянный диаграмм)'
+            }
+        }
+    )
+])
+
+

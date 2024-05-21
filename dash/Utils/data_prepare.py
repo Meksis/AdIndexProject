3fa1c67feeb56affef69ea0817bc83ccfb74aa0f
YourPersonalClown <91081376+YourPersonalClown@users.noreply.github.com>
2024-05-21 13:49:18 +0300

    add author page

--- dash/Utils/data_prepare.py
+++ dash/Utils/data_prepare.py
@@ -15,3 +15,24 @@ class Publication:
         df = df.sort_values(by='Читатели', ascending=False)
         return df.head(head)
        
+class Author:
+    
+    def f(self, df):
+        df_filtered = df
+        return df_filtered.value_counts().idxmax()
+    
+    def page_df(self, df):
+        grouped_teg = df.groupby('Автор').agg(
+            Читателей=pd.NamedAgg(column='Читатели', aggfunc='sum'),
+            кол_во_статей=pd.NamedAgg(column='post_id', aggfunc='count'),
+            лучшая_тема=pd.NamedAgg(column='post_tag', aggfunc=self.f)
+            
+            )
+        grouped_teg['Автор'] = grouped_teg.index
+        grouped_teg = grouped_teg[['Автор','Читателей','кол_во_статей','лучшая_тема']]
+        
+        return grouped_teg
+    
+    def bar_data(self, df, head):
+        df = df.sort_values(by='Читатели', ascending=False)
+        return df.head(head)
---
+++ dash/pages/author.py
@@ -0,0 +1,80 @@
+from dash import Dash, dcc, html, Input, Output, callback
+import dash
+from dash.dash_table import DataTable
+# import dash_core_components as dcc
+# import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+from Utils.data_prepare import Author
+
+dash.register_page(__name__, path = '/author')
+
+render_df = Author()
+
+# Создайте датафрейм Pandas
+df = pd.read_csv('csvs\AdIndex main news METRICS 2023-12-14.csv', index_col=0, parse_dates=True)
+
+cl_df = render_df.page_df(df=df)
+
+# Создайте приложение Dash
+# app = dash.Dash(__name__)
+
+# Определите layout
+layout = html.Div([
+    
+    html.H2('Рейтинг публикаций'),
+    html.P('Временной промежуток'),
+    
+    dcc.RadioItems(
+                ['3 Дня', '7 Дней', '30 Дней',],
+                # 'Linear',
+                value = '3 Дня',  
+                id ='date-select',
+                inline =True
+            ),
+    
+    html.P('Топ'),
+    
+    dcc.RadioItems(
+                [5, 10, 20,],
+                # 'Linear',
+                id='news-head',
+                value = 5,  
+
+                inline=True
+            ),
+    
+    dcc.Graph(
+        id='bar-author',
+        # figure= px.bar(render_df.bar_data(df,5), x='post_id', y='Читатели', title='Рейтинг публикаций', 
+        #     width=1400, height=800)
+
+    ),
+    
+
+
+    # Таблица
+    DataTable(
+                id='main-table',
+                data=cl_df.to_dict('records'),
+                sort_action='native', 
+                editable=True,
+                # row_selectable="single",
+                # filter_action='native',
+                # row_deletable=True,
+                page_size=10, 
+    
+            ),
+])
+
+
+@callback(
+    Output('bar-author', 'figure'),
+    Input('news-head','value'),
+)
+def update_figure(head):
+    fig = px.bar(render_df.bar_data(df,head), x='Автор', y='Читатели', title='Рейтинг публикаций', 
+            width=1200, height=550)
+    
+    return fig
